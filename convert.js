3d72f6cfa93350e802c38dc6948623bbace54d69
horacio9a <horacio9a@gmail.com>
2019-02-23 10:32:25 +0100

    v.3.0.3

--- convert.js
+++ convert.js
@@ -1,4 +1,4 @@
-// MyFreeCams Recorder v.3.0.1
+// MyFreeCams Recorder v.3.0.3
 
 'use strict';
 
@@ -15,10 +15,10 @@ var Queue = require('promise-queue');
 var filewalker = require('filewalker');
 var JSONStream = require('JSONStream');
 
-var config = yaml.safeLoad(fs.readFileSync('convert.yml','utf8'));
+var config = yaml.safeLoad(fs.readFileSync('convert.yml', 'utf8'));
 
-var srcDirectory = path.resolve(config.srcDirectory || 'C:/Videos/MFC');
-var dstDirectory = path.resolve(config.dstDirectory || 'C:/Videos/MFC/Converted');
+var srcDirectory = path.resolve(config.srcDirectory || './complete');
+var dstDirectory = path.resolve(config.dstDirectory || './converted');
 var dirScanInterval = config.dirScanInterval || 300;
 var maxConcur = config.maxConcur || 1;
 
@@ -34,38 +34,85 @@ function printErrorMsg(msg) {console.log(colors.gray('[' + getCurrentTime() + ']
 
 function getFiles() {let files = [];
 
-  return new Promise((resolve, reject) => {filewalker(srcDirectory, { maxPending: 1, matchRegExp: /(\.ts|\.flv)$/ })
+  return new Promise((resolve, reject) => {
+    filewalker(srcDirectory, { maxPending: 1, matchRegExp: /(\.ts|\.flv|\.mp4)$/ })
       .on('file', (p, stats) => {
         // select only "not hidden" files and not empty files (>10KBytes)
         if (!p.match(/(^\.|\/\.)/) && stats.size > 10240) {
           // push path relative to srcDirectory
-          files.push(p)}})
-      .on('done', () => {resolve(files)})
-      .walk()})}
+          files.push(p);
+        }
+      })
+      .on('done', () => {
+        resolve(files);
+      })
+      .walk();
+  });
+}
 
 function getAudioCodec(srcFile) {
   return new Promise((resolve, reject) => {
     let audioCodec = '';
-    let spawnArguments = ['-v', 'error','-select_streams', 'a:0','-show_streams','-print_format', 'json',srcFile];
+    let spawnArguments = [
+      '-v', 'error',
+      '-select_streams', 'a:0',
+      '-show_streams',
+      '-print_format', 'json',
+      srcFile
+    ];
 
     let ffprobeProcess = childProcess.spawn('ffprobe', spawnArguments);
 
-    ffprobeProcess.stdout.pipe(JSONStream.parse('streams.0')).on('data', data => {audioCodec = data.codec_name});
+    ffprobeProcess.stdout.pipe(JSONStream.parse('streams.0')).on('data', data => {
+      audioCodec = data.codec_name;
+    });
 
     ffprobeProcess.on('close', code => {
-      if (code !== 0) {reject(`Failed to get audio codec from ${srcFile}`);
-      } else {resolve(audioCodec)}})}).timeout(5000); // 5 seconds}
+      if (code !== 0) {
+        reject(`Failed to get audio codec from ${srcFile}`);
+      } else {
+        resolve(audioCodec);
+      }
+    });
+  }).timeout(5000); // 5 seconds
+}
 
-function getSpawnArguments(srcFile, dstFile) {return getAudioCodec(srcFile).then(audioCodec => (audioCodec === 'aac')
-      ? [ // aac'-i', srcFile,'-y','-hide_banner','-loglevel', 'panic','-movflags', '+faststart','-c:v','copy','-c:a','copy','-bsf:a','aac_adtstoasc','-copyts','-start_at_zero',dstFile]
-      : [ // speex or something else'-i', srcFile,'-y','-hide_banner','-loglevel','panic','-movflags','+faststart','-c:v', 'copy','-c:a', 'aac','-b:a','64k',dstFile])}
+function getSpawnArguments(srcFile, dstFile) {
+  return getAudioCodec(srcFile)
+    .then(audioCodec => (audioCodec === 'aac')
+      ? [ // aac
+        '-i', srcFile,
+        '-y',
+        '-hide_banner',
+        '-loglevel', 'panic',
+        '-movflags', '+faststart',
+        '-c:v', 'copy',
+        '-c:a', 'copy',
+        '-bsf:a', 'aac_adtstoasc',
+        '-copyts',
+        '-start_at_zero',
+        dstFile
+      ]
+      : [ // speex or something else
+        '-i', srcFile,
+        '-y',
+        '-hide_banner',
+        '-loglevel', 'panic',
+        '-movflags', '+faststart',
+        '-c:v', 'copy',
+        '-c:a', 'aac',
+        '-b:a', '64k',
+        dstFile
+      ]
+    );
+}
 
 function convertFile(srcFile) {
   let startTs = moment();
   let src = path.join(srcDirectory, srcFile);
 
   let dstPath = path.resolve(path.dirname(path.join(dstDirectory, srcFile)));
-  let dstFile = path.basename(srcFile, path.extname(srcFile)) + '.mp4';
+  let dstFile = path.basename(srcFile, path.extname(srcFile)) + '.flv';
 
   let tempDst = path.join(dstPath, `~${dstFile}`);
   let dst = path.join(dstPath, dstFile);
@@ -87,7 +134,8 @@ function convertFile(srcFile) {
           fs.statAsync(src)
             .then(stats => {
               // remember "modification time" of original file
-              mtime = Math.ceil(stats.mtime.getTime() / 1000)})
+              mtime = Math.ceil(stats.mtime.getTime() / 1000);
+            })
             .then(() => config.deleteAfter ? fs.unlinkAsync(src) : fs.renameAsync(src, `${src}.bak`))
             .then(() => fs.renameAsync(tempDst, dst))
             .then(() => fs.utimesAsync(dst, mtime, mtime))
@@ -96,29 +144,58 @@ function convertFile(srcFile) {
 
               printMsg(`Finished ${colors.green(srcFile)} after ${colors.magenta(duration)} s`);
 
-              resolve()})
-            .catch(err => {reject(err.toString())})}})}))}
+              resolve();
+            })
+            .catch(err => {
+              reject(err.toString());
+            });
+        }
+      });
+    }));
+}
 
 function mainLoop() {
   let startTs = moment().unix();
 
   Promise
     .try(() => getFiles())
-    .then(files => new Promise((resolve, reject) => {printMsg(files.length + ' file(s) to convert');
+    .then(files => new Promise((resolve, reject) => {
+      printMsg(files.length + ' file(s) to convert');
 
-      if (files.length === 0) {resolve();
-      } else {files.forEach(file => {queue
+      if (files.length === 0) {
+        resolve();
+      } else {
+        files.forEach(file => {
+          queue
             .add(() => convertFile(file))
-            .then(() => {if ((queue.getPendingLength() + queue.getQueueLength()) === 0) {resolve()}})
-            .catch(err => {printErrorMsg(err)})})}}))
-    .catch(err => {if (err) {printErrorMsg(err)}})
-    .finally(() => {let seconds = startTs - moment().unix() + dirScanInterval;
+            .then(() => {
+              if ((queue.getPendingLength() + queue.getQueueLength()) === 0) {
+                resolve();
+              }
+            })
+            .catch(err => {
+              printErrorMsg(err);
+            });
+        });
+      }
+    }))
+    .catch(err => {
+      if (err) {
+        printErrorMsg(err);
+      }
+    })
+    .finally(() => {
+      let seconds = startTs - moment().unix() + dirScanInterval;
 
-      if (seconds < 5) {seconds = 5}
+      if (seconds < 5) {
+        seconds = 5;
+      }
 
       printMsg(`Done, will scan the folder in ${seconds} seconds`);
 
-      setTimeout(mainLoop, seconds * 1000)})}
+      setTimeout(mainLoop, seconds * 1000);
+    });
+}
 
 mkdirp.sync(srcDirectory);
 mkdirp.sync(dstDirectory);
