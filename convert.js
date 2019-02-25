4a89d14b32bf557340988050ca32b024b883c29c
horacio9a <horacio9a@gmail.com>
2019-02-25 17:57:59 +0100

    File Converter for ffmpeg v.3.4.1 and higher

--- convert.js
+++ convert.js
@@ -15,10 +15,10 @@ var Queue = require('promise-queue');
 var filewalker = require('filewalker');
 var JSONStream = require('JSONStream');
 
-var config = yaml.safeLoad(fs.readFileSync('convert.yml','utf8'));
+var config = yaml.safeLoad(fs.readFileSync(path.join(__dirname, 'convert.yml'), 'utf8'));
 
-var srcDirectory = path.resolve(config.srcDirectory || 'C:/Videos/MFC');
-var dstDirectory = path.resolve(config.dstDirectory || 'C:/Videos/MFC_converted');
+var srcDirectory = path.resolve(__dirname, config.srcDirectory || 'complete');
+var dstDirectory = path.resolve(__dirname, config.dstDirectory || 'converted');
 var dirScanInterval = config.dirScanInterval || 300;
 var maxConcur = config.maxConcur || 1;
 
@@ -26,13 +26,20 @@ Queue.configure(Promise.Promise);
 
 var queue = new Queue(maxConcur, Infinity);
 
-function getCurrentTime() {return moment().format('HH:mm:ss')}
+function getCurrentTime() {
+  return moment().format(`HH:mm:ss`);
+}
 
-function printMsg(msg) {console.log(colors.gray('[' + getCurrentTime() + ']'), msg)}
+function printMsg(msg) {
+  console.log(colors.gray(`[` + getCurrentTime() + `]`), msg);
+}
 
-function printErrorMsg(msg) {console.log(colors.gray('[' + getCurrentTime() + ']'), colors.red('[ERROR]'), msg)}
+function printErrorMsg(msg) {
+  console.log(colors.gray(`[` + getCurrentTime() + `]`), colors.red(`[ERROR]`), msg);
+}
 
-function getFiles() {let files = [];
+function getFiles() {
+  let files = [];
 
   return new Promise((resolve, reject) => {
     filewalker(srcDirectory, { maxPending: 1, matchRegExp: /(\.ts|\.flv|\.mp4)$/ })
@@ -56,11 +63,16 @@ function getAudioCodec(srcFile) {
     let spawnArguments = ['-v','error','-select_streams','a:0','-show_streams','-print_format','json',srcFile];
 
     let ffprobeProcess = childProcess.spawn('ffprobe', spawnArguments);
-
-    ffprobeProcess.stdout.pipe(JSONStream.parse('streams.0')).on('data', data => {audioCodec = data.codec_name});
+    ffprobeProcess.stdout.pipe(JSONStream.parse('streams.0')).on('data', data => {
+      audioCodec = data.codec_name;
+    });
 
     ffprobeProcess.on('close', code => {
       if (code !== 0) {
+        reject(`Failed to get audio codec from ${srcFile}`);
+      } else if (!audioCodec) {
+        fs.renameAsync(srcFile, `${srcFile}.no-audio`);
+
         reject(`Failed to get audio codec from ${srcFile}`);
       } else {
         resolve(audioCodec);
@@ -72,8 +84,8 @@ function getAudioCodec(srcFile) {
 function getSpawnArguments(srcFile, dstFile) {
   return getAudioCodec(srcFile)
     .then(audioCodec => (audioCodec === 'aac')
-      ? ['-i', srcFile,'-y','-hide_banner','-loglevel','panic','-movflags','+faststart','-c:v','copy','-c:a','copy','-bsf:a','aac_adtstoasc','-copyts','-start_at_zero',dstFile]
-      : ['-i', srcFile,'-y','-hide_banner','-loglevel','panic','-movflags','+faststart','-c:v','copy','-c:a','aac','-b:a','128k',dstFile])};
+      ? ['-i', srcFile,'-y','-hide_banner','-loglevel','panic','-c:v','copy','-c:a','copy','-bsf:a','aac_adtstoasc','-copyts','-start_at_zero',dstFile] // aac
+      : ['-i', srcFile,'-y','-hide_banner','-loglevel','panic','-c:v','copy','-c:a','aac','-b:a','128k',dstFile])}; // speex or something else
 
 function convertFile(srcFile) {
   let startTs = moment();
@@ -93,6 +105,14 @@ function convertFile(srcFile) {
 
       let ffmpegProcess = childProcess.spawn('ffmpeg', spawnArguments);
 
+      ffmpegProcess.stdout.on('data', data => {
+        printMsg(data.toString());
+      });
+
+      ffmpegProcess.stderr.on('data', data => {
+        printMsg(data.toString());
+      });
+
       ffmpegProcess.on('close', code => {
         if (code !== 0) {
           reject(`Failed to convert ${srcFile}`);
@@ -103,8 +123,19 @@ function convertFile(srcFile) {
             .then(stats => {
               // remember "modification time" of original file
               mtime = Math.ceil(stats.mtime.getTime() / 1000);
+
+              if (!config.deleteAfter) {
+                return fs.renameAsync(src, `${src}.bak`);
+              }
+
+              return fs.statAsync(tempDst).then(tempStats => {
+                if ((Math.abs(stats.size - tempStats.size) / tempStats.size) < 0.1) {
+                  return fs.unlinkAsync(src);
+                } else {
+                  return fs.renameAsync(src, `${src}.bak`);
+                }
+              });
             })
-            .then(() => config.deleteAfter ? fs.unlinkAsync(src) : fs.renameAsync(src, `${src}.bak`))
             .then(() => fs.renameAsync(tempDst, dst))
             .then(() => fs.utimesAsync(dst, mtime, mtime))
             .then(() => {
@@ -128,7 +159,7 @@ function mainLoop() {
   Promise
     .try(() => getFiles())
     .then(files => new Promise((resolve, reject) => {
-      printMsg(files.length + ' file(s) to convert');
+      printMsg(files.length + ` file(s) to convert`);
 
       if (files.length === 0) {
         resolve();
@@ -136,13 +167,13 @@ function mainLoop() {
         files.forEach(file => {
           queue
             .add(() => convertFile(file))
-            .then(() => {
+            .catch(err => {
+              printErrorMsg(err);
+            })
+            .finally(() => {
               if ((queue.getPendingLength() + queue.getQueueLength()) === 0) {
                 resolve();
               }
-            })
-            .catch(err => {
-              printErrorMsg(err);
             });
         });
       }
